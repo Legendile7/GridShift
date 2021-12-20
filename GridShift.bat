07b13631c0e5e4bb510bb51ee98bec96ed82d611
Legendile7 <56519032+Legendile7@users.noreply.github.com>
2021-12-20 18:29:38 -0500

    Update GridShift.bat

--- GridShift.bat
+++ GridShift.bat
@@ -1,6 +1,7 @@
 COLOR 0b
 @echo off
 cls
+echo GridShift Cipher v1.10.3 (December 20, 2021) Copyright 2021 by Divesh Gupta is licensed under CC BY-NC-ND 4.0. To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/4.0/
 :start
 echo.
 java Main.java
